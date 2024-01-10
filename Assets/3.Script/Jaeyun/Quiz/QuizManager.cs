363f3568ca8a92341118d55c4a521e827f792f3c
Jaeyun Lee <ghghwodbs@gmail.com>
2024-01-11 01:09:04 +0900

    [Fix] QuizManager

--- Assets/3.Script/Jaeyun/Quiz/QuizManager.cs
+++ Assets/3.Script/Jaeyun/Quiz/QuizManager.cs
@@ -24,8 +24,7 @@ public class QuizManager : MonoBehaviour
     [Header("Data")]
     public NPCInfoSetting npcInfoSet; // npc에 따른 동물 이름 정보 가져오기
     List<Dictionary<string, object>> animalData;
-    private int dataIndex = 0;
-    private int quizIndex = 0;
+    int quizIndex = 0;
 
     private Touch touch;
     [SerializeField] private LayerMask layer;
@@ -56,6 +55,8 @@ public class QuizManager : MonoBehaviour
 
     public void AnimalsQuiz_Print()
     {
+        int dataIndex = 0;
+
         for (int i = 0; i < animalData.Count; i++)
         {
             if (animalData[i]["Animal"].ToString().Trim().Equals(npcInfoSet.npcInfo.npcName.Trim()))
@@ -64,22 +65,52 @@ public class QuizManager : MonoBehaviour
                 break;
             }
         }
-        animalText.text = animalData[dataIndex]["Animal"].ToString(); // npc에 따른 animal name
         string[] array = animalData[dataIndex]["Quiz"].ToString().Split(']'); // 해당 animal quiz 순서대로
-        quizText.text = array[quizIndex];
-        quizIndex++;
+        contentsText.text = array[quizIndex];
 
         if (quizIndex.Equals(array.Length))
         {
             clearMenu.SetActive(true);
             mainMenu.SetActive(false);
-            
+            quizIndex = 0;
         }
     }
 
     public void Quiz_Result()
-    {
+    { // quiz 맞다, 아니다를 누른 뒤 나오는 obj
+        int dataIndex = 0;
+        for (int i = 0; i < animalData.Count; i++)
+        {
+            if (animalData[i]["Animal"].ToString().Trim().Equals(npcInfoSet.npcInfo.npcName.Trim()))
+            {
+                dataIndex = i;
+                break;
+            }
+        }
+        string[] array = animalData[dataIndex]["Answer"].ToString().Split(']'); // 해당 animal quiz 순서대로
+        quizText.text = array[quizIndex];
+    }
 
+    public void Quiz_Result_Button(string answer)
+    {
+        int dataIndex = 0;
+        int rightCount = 0;
+        for (int i = 0; i < animalData.Count; i++)
+        {
+            if (animalData[i]["Animal"].ToString().Trim().Equals(npcInfoSet.npcInfo.npcName.Trim()))
+            {
+                dataIndex = i;
+                break;
+            }
+        }
+        string[] array = animalData[dataIndex]["Info"].ToString().Split(']'); // 해당 animal quiz 순서대로
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Equals(answer))
+            {
+                rightCount++;
+            }
+        }
     }
 
     public void Quiz_Reward()
@@ -96,42 +127,50 @@ public class QuizManager : MonoBehaviour
                 touch = Input.GetTouch(0);
                 if (touch.phase == TouchPhase.Ended)
                 {
-                    if (!talk_pannel.activeSelf)
+                    if (!mainMenu.activeSelf && !plusMenu.activeSelf && !clearMenu.activeSelf)
                     {
-                        touched_pos = Camera.main.ScreenToWorldPoint(touch.position);
                         Try_raycast(touch.position);
-                        StudyManager.instance.Try_raycast(touch.position);
                     }
-                    else
+                    else if (mainMenu.activeSelf)
                     {
-                        if (nextText)
-                        { // npc info text
-                            dialog_text.text = $"<#ABABAB>{npcInfoSet.npcInfo.npcText}</color>";
-                            micButton.SetActive(npcInfoSet.npcInfo.micButton); // audio button을 사용하는 지
-                        }
+                        quizIndex++;
+                    }
+                    else if (plusMenu.activeSelf)
+                    {
+
+                    }
+                    else if (clearMenu.activeSelf)
+                    {
+
                     }
                 }
             }
         }
-
         else
         { // Window
             if (Input.GetMouseButtonUp(0))
             {
-                if (!talk_pannel.activeSelf)
+                if (!mainMenu.activeSelf)
                 {
-                    mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     Try_raycast(Input.mousePosition);
-                    StudyManager.instance.Try_raycast(Input.mousePosition);
                 }
                 else
                 {
-                    if (nextText)
-                    { // npc info text
-                        dialog_text.text = $"<#ABABAB>{npcInfoSet.npcInfo.npcText}</color>";
-                        micButton.SetActive(npcInfoSet.npcInfo.micButton); // audio button을 사용하는 지
-                    }
+                    quizIndex++;
                 }
+            }
+        }
+    }
+
+    private void Try_raycast(Vector3 pos)
+    { // NPC 찾는 raycast
+        Ray ray = Camera.main.ScreenPointToRay(pos);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layer))
+        {
+            if (hit.collider.CompareTag("NPC"))
+            { // npc일 때
+                npcInfoSet = hit.collider.gameObject.GetComponent<NPCInfoSetting>();
+                AnimalsQuiz_Print();
             }
         }
     }
