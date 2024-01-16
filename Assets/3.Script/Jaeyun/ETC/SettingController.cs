a42cd4c9caf1b85074148caddc7f65cf90240d62
Jaeyun Lee <ghghwodbs@gmail.com>
2024-01-17 01:26:30 +0900

    [Fix] UI Setting fix

--- Assets/3.Script/Jaeyun/ETC/SettingController.cs
+++ Assets/3.Script/Jaeyun/ETC/SettingController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class UserSettingData
+{
+    public double bgmValue;
+    public double sfxValue;
+    public int qualitySet;
+    public UserSettingData(double bgm, double sfx, int quality)
+    {
+        bgmValue = bgm;
+        sfxValue = sfx;
+        qualitySet = quality;
+    }
+}
+
 public class SettingController : MonoBehaviour
 { // Cavase Audio Setting에 넣어주기
     public Slider bgmSlider, sfxSlider;
-
-    private void Awake()
-    {
-        // json file settign load
-    }
+    private int quality = AudioManager.instance.qualitySet;
 
     public void MuteBGM()
     {
@@ -25,25 +34,30 @@ public class SettingController : MonoBehaviour
     public void BGMVolume()
     {
         AudioManager.instance.BGMVolume(bgmSlider.value);
+        AudioManager.instance.UpdateData(AudioManager.instance.settingPath, bgmSlider.value, sfxSlider.value, quality);
     }
 
     public void SFXVolume()
     {
         AudioManager.instance.SFXVolume(sfxSlider.value);
+        AudioManager.instance.UpdateData(AudioManager.instance.settingPath, bgmSlider.value, sfxSlider.value, quality);
     }
 
     public void LowQuality()
     {
         QualitySettings.SetQualityLevel(0);
+        AudioManager.instance.UpdateData(AudioManager.instance.settingPath, bgmSlider.value, sfxSlider.value, 0);
     }
 
     public void MiddleQuality()
     {
         QualitySettings.SetQualityLevel(1);
+        AudioManager.instance.UpdateData(AudioManager.instance.settingPath, bgmSlider.value, sfxSlider.value, 1);
     }
 
     public void HighQuality()
     {
         QualitySettings.SetQualityLevel(2);
+        AudioManager.instance.UpdateData(AudioManager.instance.settingPath, bgmSlider.value, sfxSlider.value, 2);
     }
 }
